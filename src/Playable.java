d2f56f2d69fbfbba88f53b0f7da1e0cd987330bf
Mira <joseph.pratt@case.edu>
2019-01-06 11:20:05 -0500

    Made Playable class

--- src/Playable.java
+++ src/Playable.java
@@ -1,7 +1,21 @@
 
-public class Playable extends Unit {
+
+public class Playable extends Unit{
 
 	private int[] inven;
+	
+	Playable(int[] inven, int hp, int mp, int melee, int magic, int speed, int physdef, int magdef){
+		super( hp, mp, melee, magic, speed, physdef, magdef);
+		this.inven = inven;
+	}
+
+	public int[] getInven() {
+		return inven;
+	}
+
+	public void setInven(int[] inven) {
+		this.inven = inven;
+	}
 	
 	
 }
